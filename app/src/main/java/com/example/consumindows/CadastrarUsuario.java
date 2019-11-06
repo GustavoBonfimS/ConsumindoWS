cd7c8f07ae34ace2001584f216c3caeceef276a9
Gustavo Bonfim <gunais1234@gmail.com>
2019-11-05 22:07:14 -0300

    tratando erros CadastroUsuairo

--- app/src/main/java/com/example/consumindows/CadastrarUsuario.java
+++ app/src/main/java/com/example/consumindows/CadastrarUsuario.java
@@ -22,7 +22,7 @@ public class CadastrarUsuario extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_usuario);
 
-        Button btncadastrar = findViewById(R.id.btnCadastrar);
+        final Button btncadastrar = findViewById(R.id.btnCadastrar);
         final EditText email = findViewById(R.id.etLogin);
         final EditText username = findViewById(R.id.etUsername);
         final EditText senha = findViewById(R.id.etSenha);
@@ -37,28 +37,27 @@ public class CadastrarUsuario extends AppCompatActivity {
                 c.setLogin(username.getText().toString());
                 c.setSenha(senha.getText().toString());
 
-                /*
+
                 Call<Cliente> call = new RetrofitConfig().getWigService().cadastrarCliente(c);
                 call.enqueue(new Callback<Cliente>() {
                     @Override
                     public void onResponse(Call<Cliente> call, Response<Cliente> response) {
                         if (response.body() != null) {
                             c.setIdcliente(response.body().getIdcliente());
+                            Toast.makeText(CadastrarUsuario.this, "Sucesso! id=" + response.body().getIdcliente(),
+                                    Toast.LENGTH_SHORT).show();
 
-                            c.setIdusuario(response.body().getIdusuario());
-                            c.setPerfil(response.body().getPerfil());
-                            c.setLogin(response.body().getLogin());
-                            c.setSenha(response.body().getSenha());
-                            c.setEmail(response.body().getEmail());
+                            btncadastrar.setVisibility(View.INVISIBLE); //tentativa de esconder o botão depois do click
                         }
                     }
 
                     @Override
                     public void onFailure(Call<Cliente> call, Throwable t) {
-                        Log.e("Wig", "Erro ao fazer request");
+                        Log.e("Wig", "Erro ao fazer request: " + t.getMessage());
+                        Toast.makeText(CadastrarUsuario.this, "ERRO: " + t.getMessage(), Toast.LENGTH_SHORT).show();
                     }
-                }); */
-                Toast.makeText(CadastrarUsuario.this, "id= " + c.getIdcliente(), Toast.LENGTH_SHORT).show();
+                });
+                // Toast.makeText(CadastrarUsuario.this, "id= " + c.getIdcliente(), Toast.LENGTH_SHORT).show();
 
             }
         });
