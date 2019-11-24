e0c35a1313491d7f59522bd22e9016832bae3ac8
Gustavo Bonfim <gunais1234@gmail.com>
2019-11-24 14:12:38 -0300

    fix git bug

--- app/src/main/java/com/example/consumindows/loginActivity.java
+++ app/src/main/java/com/example/consumindows/loginActivity.java
@@ -1,6 +1,8 @@
 package com.example.consumindows;
+
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -11,6 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import modelo.RetrofitConfig;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -18,96 +21,98 @@ import retrofit2.Response;
 
 public class loginActivity extends AppCompatActivity {
 
-  private EditText etLogin;
-  private EditText etSenha;
+    private EditText etLogin;
+    private EditText etSenha;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    getSupportActionBar().hide(); // esconde barra de titulo
-    setContentView(R.layout.activity_login);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide(); // esconde barra de titulo
+        setContentView(R.layout.activity_login);
 
-    etLogin = findViewById(R.id.etLogin);
-    etSenha = findViewById(R.id.etSenha);
+        etLogin = findViewById(R.id.etLogin);
+        etSenha = findViewById(R.id.etSenha);
 
-    final Button btnLogin = findViewById(R.id.btnLogin);
-    final TextView cadastrar = findViewById(R.id.tvCadastrar);
+        final Button btnLogin = findViewById(R.id.btnLogin);
+        final TextView cadastrar = findViewById(R.id.tvCadastrar);
 
-    btnLogin.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        validaCampos();
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!validaCampos()) {
 
-        Call<String> call = new RetrofitConfig().getWigService()
-                .validarLogin(etLogin.getText().toString()
-                        , etSenha.getText().toString());
-        call.enqueue(new Callback<String>() {
-          @Override
-          public void onResponse(Call<String> call, Response<String> response) {
-            // status.setText(response.body());
+                    Call<String> call = new RetrofitConfig().getWigService()
+                            .validarLogin(etLogin.getText().toString()
+                                    , etSenha.getText().toString());
+                    call.enqueue(new Callback<String>() {
+                        @Override
+                        public void onResponse(Call<String> call, Response<String> response) {
+                            // status.setText(response.body());
 
-            if (response.body().equals("true")) {
-              Intent telaMainTeste = new Intent(loginActivity.this, testeIntente.class);
-              Bundle param = new Bundle();
-              param.putString("login", etLogin.getText().toString());
+                            if (response.body().equals("true") && response.isSuccessful()) {
+                                Intent telaMainTeste = new Intent(loginActivity.this, Index.class);
+                                Bundle param = new Bundle();
+                                param.putString("login", etLogin.getText().toString());
 
-              telaMainTeste.putExtras(param);
-              startActivity(telaMainTeste);
-            } else {
-              Toast.makeText(loginActivity.this, "Nome de usuairo ou senha icnorretos",
-                      Toast.LENGTH_SHORT).show();
+                                telaMainTeste.putExtras(param);
+                                startActivity(telaMainTeste);
+                            } else {
+                                Toast.makeText(loginActivity.this, "Nome de usuairo ou senha icnorretos",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<String> call, Throwable t) {
+                            Log.e("wig", "erro ao se comunicar com o WS: " + t.getMessage());
+                        }
+                    });
+                }
             }
-          }
-
-          @Override
-          public void onFailure(Call<String> call, Throwable t) {
-            Log.e("wig", "erro ao se comunicar com o WS: " + t.getMessage());
-          }
         });
-      }
-    });
 
-    // click no texto de cadastrar-se
-    cadastrar.setOnClickListener(new View.OnClickListener() {
+        // click no texto de cadastrar-se
+        cadastrar.setOnClickListener(new View.OnClickListener() {
 
-      @Override
-      public void onClick(View v) {
-        Intent telaCadastro = new Intent(loginActivity.this, Index.class);
-        startActivity(telaCadastro);
-      }
-    });
+            @Override
+            public void onClick(View v) {
+                Intent telaCadastro = new Intent(loginActivity.this, Index.class);
+                startActivity(telaCadastro);
+            }
+        });
 
-  }
-
-  private void validaCampos() {
-    boolean res = false;
-    String login = etLogin.getText().toString();
-    String senha = etSenha.getText().toString();
-
-    if (res = isCampoVazio(login)) {
-      etLogin.requestFocus();
-    } else if (res = isCampoVazio(senha)) {
-      etSenha.requestFocus();
     }
 
-    if (res) {
-      AlertDialog.Builder dlg = new AlertDialog.Builder(this);
-      dlg.setTitle("Aviso");
-      dlg.setMessage("Há campos vazios ou em branco");
-      dlg.setNeutralButton("OK", null);
-      dlg.show();
+    private boolean validaCampos() {
+        boolean res = false;
+        String login = etLogin.getText().toString();
+        String senha = etSenha.getText().toString();
+
+        if (res = isCampoVazio(login)) {
+            etLogin.requestFocus();
+        } else if (res = isCampoVazio(senha)) {
+            etSenha.requestFocus();
+        }
+
+        if (res) {
+            AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+            dlg.setTitle("Aviso");
+            dlg.setMessage("Há campos vazios ou em branco");
+            dlg.setNeutralButton("OK", null);
+            dlg.show();
+        }
+        return res;
     }
-  }
 
-  private boolean isCampoVazio (String valor) {
-    boolean resultado = (TextUtils.isEmpty(valor)) || valor.trim().isEmpty();
-    return resultado;
-  }
+    private boolean isCampoVazio(String valor) {
+        boolean resultado = (TextUtils.isEmpty(valor)) || valor.trim().isEmpty();
+        return resultado;
+    }
 
-  // sera usado na classe de cadastro
-  private boolean isEmailValido (String email) {
-    boolean resultado = (!isCampoVazio(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
-    return resultado;
-  }
+    // sera usado na classe de cadastro
+    private boolean isEmailValido(String email) {
+        boolean resultado = (!isCampoVazio(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+        return resultado;
+    }
 
 }
