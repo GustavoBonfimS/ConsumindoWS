0500b2551f6bdd3f4d004c1011df8239cda1a08a
Felipe1540 <57272606+Felipe1540@users.noreply.github.com>
2019-11-27 15:29:20 -0300

    Index Layouts Criados /começo/

--- app/src/main/java/com/example/consumindows/Index.java
+++ app/src/main/java/com/example/consumindows/Index.java
@@ -44,8 +44,8 @@ public class Index extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                R.id.nav_pgInicial, R.id.nav_perfil, R.id.nav_empresas,
+                R.id.nav_infwig, R.id.nav_infwig, R.id.nav_logout)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
