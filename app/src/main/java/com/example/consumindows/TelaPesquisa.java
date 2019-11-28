8682c7f1e02dc212926454b19c64d062b86d5f23
luisc13 <luisinho0903@gmail.com>
2019-11-28 04:12:38 -0300

    TelaPesquisa

--- app/src/main/java/com/example/consumindows/TelaPesquisa.java
+++ app/src/main/java/com/example/consumindows/TelaPesquisa.java
@@ -1,6 +1,8 @@
 package com.example.consumindows;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,20 @@ public class TelaPesquisa extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_pesquisa);
+
+        SearchView pesquisar = (SearchView) findViewById(R.id.search);
+        pesquisar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
     }
+
 }
