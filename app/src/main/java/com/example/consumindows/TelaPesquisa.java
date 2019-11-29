617fbac0e83e475c4a1117d9fa83e18e27472b85
Gustavo Bonfim <gunais1234@gmail.com>
2019-11-29 18:09:53 -0300

    pesquisa: tentando listar

--- app/src/main/java/com/example/consumindows/TelaPesquisa.java
+++ app/src/main/java/com/example/consumindows/TelaPesquisa.java
@@ -1,8 +1,12 @@
 package com.example.consumindows;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,31 +19,50 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class TelaPesquisa extends AppCompatActivity {
+    ListView listView;
 
     public void onCreate(Bundle savedInstanceState) {
         getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_pesquisa);
 
+        listView = findViewById(R.id.lvLista);
 
         final SearchView pesquisar = (SearchView) findViewById(R.id.search);
         pesquisar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
+                if (s != null) {
+                    Call<List<Empresa>> call = new RetrofitConfig().getWigService().pesquisa(s);
+                    call.enqueue(new Callback<List<Empresa>>() {
+                        @Override
+                        public void onResponse(Call<List<Empresa>> call, Response<List<Empresa>> response) {
+                            if (response.code() == 200 || response.isSuccessful()) {
+                                List<Empresa> lista = response.body();
+
+                                String[] array = new String[lista.size()];
+                                for (int i = 0; i < lista.size(); i++) {
+                                    array[i] = lista.get(i).getLogin();
+                                }
+
+                                ArrayAdapter<String> adapter = new ArrayAdapter<String>(TelaPesquisa.this,
+                                        android.R.layout.simple_list_item_1, array);
+                                listView.setAdapter(adapter);
+
+                            }
+
+                        }
+
+                        @Override
+                        public void onFailure(Call<List<Empresa>> call, Throwable t) {
+                            Log.e("wig", "erro ao se comunicar com o WS: " + t.getMessage());
+                        }
+                    });
+                }
+
+
+                Toast.makeText(TelaPesquisa.this, "pesquisou: " + s, Toast.LENGTH_SHORT).show();
                 return false;
-                Call<List<Empresa>> call new RetrofitConfig().getWigService().pesquisa();
-                        call.enqueue(new Callback<List<Empresa>>() {
-                            @Override
-                            public void onResponse(Call<List<Empresa>> call, Response<List<Empresa>> response) {
-
-                            }
-
-                            @Override
-                            public void onFailure(Call<List<Empresa>> call, Throwable t) {
-
-                            }
-                        });
-
             }
 
             @Override
