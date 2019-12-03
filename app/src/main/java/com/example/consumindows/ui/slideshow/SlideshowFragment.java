5e5307a13874e55739c1f863b866a21f2636e248
Gustavo Bonfim <gunais1234@gmail.com>
2019-12-03 02:08:37 -0300

    add splash screen

--- app/src/main/java/com/example/consumindows/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/consumindows/ui/slideshow/SlideshowFragment.java
@@ -1,5 +1,6 @@
 package com.example.consumindows.ui.slideshow;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,6 +13,7 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.view.SupportActionModeWrapper;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -30,6 +32,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class SlideshowFragment extends Fragment {
+
 
     private SlideshowViewModel slideshowViewModel;
     Empresa empresa;
