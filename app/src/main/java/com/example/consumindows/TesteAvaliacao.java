3afdf0c7a455ef64657feef2986d9fc5e65afd78
Gustavo Bonfim <gunais1234@gmail.com>
2019-12-07 19:27:12 -0300

    tela de avaliacao: ok / botao para voltar

--- app/src/main/java/com/example/consumindows/TesteAvaliacao.java
+++ app/src/main/java/com/example/consumindows/TesteAvaliacao.java
@@ -37,6 +37,7 @@ public class TesteAvaliacao extends AppCompatActivity {
         setContentView(R.layout.activity_teste_avaliacao);
         etAutor = findViewById(R.id.etAutor);
         final Button btAvalia = findViewById(R.id.btnAvaliacao);
+        final Button btnVoltar = findViewById(R.id.btnVoltar);
         etConteudo = findViewById(R.id.etConteudo);
         final Avaliacao a = new Avaliacao();
 
@@ -46,50 +47,57 @@ public class TesteAvaliacao extends AppCompatActivity {
         btAvalia.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                validaCampos();
+                if (!validaCampos()) {
 
-                a.setAutor(etAutor.getText().toString());
-                a.setConteudo(etConteudo.getText().toString());
-                a.setIdcliente(cliente.getIdcliente());
-                a.setIdempresa(empresa.getIdempresa());
+                    a.setAutor(etAutor.getText().toString());
+                    a.setConteudo(etConteudo.getText().toString());
+                    a.setIdcliente(cliente.getIdcliente());
+                    a.setIdempresa(empresa.getIdempresa());
 
-                // pega data atual do sistema
-                java.util.Date dataUtil = new java.util.Date();
-                Date data = new Date(dataUtil.getTime());
-                a.setData(data);
+                    // pega data atual do sistema
+                    java.util.Date dataUtil = new java.util.Date();
+                    Date data = new Date(dataUtil.getTime());
+                    a.setData(data);
 
-                // pegar hora atual do sistema;
-                Time hora = new Time(dataUtil.getTime());
-                a.setHora(hora);
+                    // pegar hora atual do sistema;
+                    Time hora = new Time(dataUtil.getTime());
+                    a.setHora(hora);
 
-                Call<Avaliacao> call = new RetrofitConfig().getWigService().inserirAvaliacao(a);
-                call.enqueue(new Callback<Avaliacao>() {
-                    @Override
-                    public void onResponse(Call<Avaliacao> call, Response<Avaliacao> response) {
-                        if (response.code() == 200 || response.isSuccessful()) {
-                            Toast.makeText(TesteAvaliacao.this, "Avaliado com sucesso!"
-                                    , Toast.LENGTH_SHORT).show();
-                            a.setIdavaliacao(response.body().getIdavaliacao());
-                            a.setHora(response.body().getHora());
-                            a.setData(response.body().getData());
-                            btAvalia.setVisibility(View.GONE);
-                            //finish();
+                    Call<Avaliacao> call = new RetrofitConfig().getWigService().inserirAvaliacao(a);
+                    call.enqueue(new Callback<Avaliacao>() {
+                        @Override
+                        public void onResponse(Call<Avaliacao> call, Response<Avaliacao> response) {
+                            if (response.code() == 200 || response.isSuccessful()) {
+                                Toast.makeText(TesteAvaliacao.this, "Avaliado com sucesso!"
+                                        , Toast.LENGTH_SHORT).show();
+                                a.setIdavaliacao(response.body().getIdavaliacao());
+                                a.setHora(response.body().getHora());
+                                a.setData(response.body().getData());
+                                btAvalia.setVisibility(View.GONE);
+                                btnVoltar.setVisibility(View.VISIBLE);
+                                //finish();
 
+                            }
                         }
-                    }
 
-                    @Override
-                    public void onFailure(Call<Avaliacao> call, Throwable t) {
-                        Log.e("wig", "erro ao fazer request" + t.getMessage());
-                    }
-                });
+                        @Override
+                        public void onFailure(Call<Avaliacao> call, Throwable t) {
+                            Log.e("wig", "erro ao fazer request" + t.getMessage());
+                        }
+                    });
 
-
+                }
                 // Toast.makeText(getBaseContext(), "id= :" + a.getIdavaliacao(), Toast.LENGTH_SHORT).show();
             } // onClick ends
 
         });
 
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
     }
 
